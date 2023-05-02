ec3f914440a0ff21cc574d80fb255d4e46e9e74f
Moises.alves <Moises.alves@atp.com.br>
2023-05-02 19:17:43 -0300

    Entendendo os controllers e o MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(){
+            return "Hello World";
+        }
+    }
+}
