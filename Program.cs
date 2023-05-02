b322482e4578a14d0d376a6cc101612376f9c9a6
Moises.alves <Moises.alves@atp.com.br>
2023-05-02 09:25:26 -0300

    Mapeando uma req

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/jamelao", () => "Jamelão é muito bom!");
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
