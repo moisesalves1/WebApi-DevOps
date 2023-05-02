2d19452272ec5deb54586bd628867154e7436f77
Moises.alves <Moises.alves@atp.com.br>
2023-05-02 20:00:12 -0300

    Lendo itens do banco

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
@@ -6,8 +8,10 @@ namespace Todo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public string Get(){
-            return "Hello World";
+        public List<TodoModel> Get(
+            [FromServices] AppDbContext context
+        ){
+            return context.Todos.ToList();
         }
     }
 }
