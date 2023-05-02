1b4e3396b1e1aad0ebd941280606494e906677be
Moises.alves <Moises.alves@atp.com.br>
2023-05-02 09:38:31 -0300

    Funções anônimas

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/jamelao", () => "Jamelão é muito bom!");
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => 
+{
+    return "Hello World!";
+});
 
 app.Run();
