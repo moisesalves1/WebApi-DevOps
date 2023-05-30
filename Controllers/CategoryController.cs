541622fa50abfcd0c3d7630894d20ebcf1f891ed
Moises Alves <moisesalves1@gmail.com>
2023-05-30 17:48:27 -0300

    Async e Await

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,16 +1,17 @@
 ﻿using Blog.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        [HttpGet("categories")]
-        public IActionResult Get(
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync(
             [FromServices]BlogDataContext context)
         {
-            var categories = context.Categories.ToList();
+            var categories = await context.Categories.ToListAsync();
             return Ok(categories);
         }
     }
