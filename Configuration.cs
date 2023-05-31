038b3333bc9754cda4446552e90ab9190e3fd119
Moises Alves <moisesalves1@gmail.com>
2023-05-31 12:39:25 -0300

    Implementando ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,6 +2,10 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDh1MTk3YjkyODdkNDkyYjcwOGU=";
+        public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDh1MTk3YjkyODdkNDkyYjcwOGU=";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_ILTevUK/0ey3NaCV/un8g=";
+
+
     }
 }
